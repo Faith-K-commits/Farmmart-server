1b7516fa4c0ca8b7245e03566bcb9ba29e56a344
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 21:35:14 +0300

    update deployment service name and add index route to app

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ from config import db, app, api
 # Secret key for sessions
 app.config['SECRET_KEY'] = 'secret_key'
 
+@app.route('/')
+def index():
+    return 'Deployed!', 200
+
 if __name__ == '__main__':
     app.run(debug=True)
