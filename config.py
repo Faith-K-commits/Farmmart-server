d614539a2f0dee8139942d8ca918d744f6816a83
root <root@DESKTOP-RHA5CJR.localdomain>
2024-11-17 10:45:07 +0300

    update login resource

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, current_app
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -19,6 +19,8 @@ def config():
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret_key'
+
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
